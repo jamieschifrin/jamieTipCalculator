817c9886f824d215d5ffd20ebbdac76b1d548133
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-23 11:17:54 -0400

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+function tipCalculate() {
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numberOfPeople = document.getElementById("numberOfPeople").value;
+    
+    var total = (billAmount * serviceQuality) / numberOfPeople;
+    total = total.toFixed(2);
+
+    document.getElementById("totalTipAmount").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+    document.getElementById("calculate").onclick = function () {
+        tipCalculate();
+
+}
